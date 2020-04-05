f0915e87c8961cec91d35e1f1f30b20b08e3338e
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-04-05 13:15:42 -0400

    Gradle: Enable Junit Platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ repositories {
     mavenCentral()
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
